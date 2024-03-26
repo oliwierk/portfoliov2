b0b60f0580402ee77caea4a8bfc5fffd3c65df17
oliwierk <oliwier.kotlicki@gmail.com>
2024-03-26 21:49:58 +0100

    tablet view mini fix

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { gsap } from "gsap";
-
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
@@ -47,4 +48,46 @@ gsap.from(".signature", {
 	y: 50, // Startowe przesunięcie w pionie
 	duration: 1, // Czas trwania animacji w sekundach
 	ease: "power1.out", // Typ wygładzania
+});
+document.addEventListener("DOMContentLoaded", event => {
+	// Inicjalizacja sceny, kamery i renderera
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	const canvas = document.getElementById("myCanvas");
+	const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+	// Dodaj oświetlenie
+	const light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.set(2, 2, 5);
+	scene.add(light);
+
+	// Wczytaj model twarzy
+	const loader = new GLTFLoader();
+	loader.load(
+		"/skeleton/scene.gltf",
+		function (gltf) {
+			scene.add(gltf.scene);
+		},
+		undefined,
+		function (error) {
+			console.error(error);
+		}
+	);
+
+	// Ustaw pozycję kamery
+	camera.position.z = 5;
+
+	// Funkcja animująca
+	function animate() {
+		requestAnimationFrame(animate);
+		renderer.render(scene, camera);
+	}
+	animate();
 });
