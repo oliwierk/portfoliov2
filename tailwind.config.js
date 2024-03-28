e12c24fb5cd28196ca1b1bfa3908f467ce30c0ff
oliwierk <oliwier.kotlicki@gmail.com>
2024-03-28 14:21:58 +0100

    minor chan ges in placeholder, tooltips on navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage: {
-				"time-bus": "url('/TimeBus.jpg')",
+				"portfolio-v1": "url('/portfoliov1.png')",
+				"lorem-bg": "url('/lorem.png')",
 			},
 		},
 	},
